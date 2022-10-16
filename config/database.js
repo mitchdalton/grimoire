3568932fdca9fe236603cda9f8448b0274bf7632
dalton05 <dalton05@gmail.com>
2022-10-16 17:52:17 -0600

    all new styling

--- config/database.js
+++ config/database.js
@@ -3,14 +3,23 @@ const uri = process.env.MONGODB_URI;
 
 const connectDB = async () => {
   try {
-    console.log(uri);
-    const conn = await mongoose.connect(uri, {
+
+    // TO DEPLOY, COMMENT THIS OUT AND UNCOMMENT BELOW
+    const conn = await mongoose.connect(process.env.DB_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
       useCreateIndex: true
-    })
+    }) 
+    // const conn = await mongoose.connect(uri, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    //   useFindAndModify: false,
+    //   useCreateIndex: true
+    // })
 
+
+    
     console.log(`MongoDB Connected: ${conn.connection.host}`)
   } catch (err) {
     console.error(err)
